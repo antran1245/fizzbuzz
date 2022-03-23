8f94e8409a3dce10cb71fe6e21c0176490b4ce95
antran1245 <antran1245@gmail.com>
2022-03-23 16:37:24 -0700

    move comments

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 for(let i = 1; i < 101; i++) {
     if((i%3 == 0) && (i%5 == 0)) { //check for multiple of 3 and 5
         console.log("FizzBuzz");
-    } else if (i%3 == 0) {
-        console.log("Fizz"); //check for multiple of 3
-    } else if (i%5 == 0) {
-        console.log("Buzz"); //check for multiple of 5
-    } else {
-        console.log(i); //not multiple of 3 and 5
+    } else if (i%3 == 0) { //check for multiple of 3
+        console.log("Fizz"); 
+    } else if (i%5 == 0) { //check for multiple of 5
+        console.log("Buzz"); 
+    } else { //not multiple of 3 and 5
+        console.log(i); 
     }
 
 }
